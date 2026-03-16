0521cf7a7dfac55b8ac066229c1841f6f37e77c6
Miao Wang <shankerwangmiao@gmail.com>
2026-03-17 02:34:19 +0800

    Revert "git-fcgi: add buffering proxy to quickly read from fcgi"
    This reverts commit c9dcd12d75f9d85017341981d266743df5750972.

--- git-fcgi/cmd.sh
+++ git-fcgi/cmd.sh
@@ -2,18 +2,10 @@
 
 THREADS=${THREADS:-"64"}
 QUEUE_SIZE=${QUEUE_SIZE:-"48"}
-MAX_BUFFER_SIZE=${MAX_BUFFER_SIZE:-"1G"}
-BUFFER_PATH=${BUFFER_PATH:-"/tmp"}
-
 export HOME=/var/www
 (
     while true; do
         /go-queue --queue-size "${QUEUE_SIZE}" --port-number 8888
     done
 )&
-(
-    while true; do
-        /buffering-proxy --listen ":5000" --max-buffer-size "${MAX_BUFFER_SIZE}" --on-disk-buffer-path "${BUFFER_PATH}" --upstream "unix:/tmp/fcgi.sock"
-    done
-)&
-exec /usr/bin/spawn-fcgi -u www-data -g www-data -s /tmp/fcgi.sock -n -- /usr/bin/multiwatch -f "${THREADS}" -- /usr/sbin/fcgiwrap
+exec /usr/bin/spawn-fcgi -u www-data -g www-data -p 5000 -n -- /usr/bin/multiwatch -f ${THREADS} -- /usr/sbin/fcgiwrap
