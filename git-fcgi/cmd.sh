15588fa068657496b8a9b9a3536dc2434f01732f
Miao Wang <shankerwangmiao@gmail.com>
2026-04-25 17:07:41 +0800

    go-queue: check if memory is under watermark

--- git-fcgi/cmd.sh
+++ git-fcgi/cmd.sh
@@ -2,10 +2,11 @@
 
 THREADS=${THREADS:-"64"}
 QUEUE_SIZE=${QUEUE_SIZE:-"48"}
+MEMORY_WATERMARK=${MEMORY_WATERMARK:-"536870912"} # 512 MiB
 export HOME=/var/www
 (
     while true; do
-        /go-queue --queue-size "${QUEUE_SIZE}" --port-number 8888
+        /go-queue --queue-size "${QUEUE_SIZE}" --port-number 8888 --memory-watermark "${MEMORY_WATERMARK}"
     done
 )&
 exec /usr/bin/spawn-fcgi -u www-data -g www-data -p 5000 -n -- /usr/bin/multiwatch -f ${THREADS} -- /usr/sbin/fcgiwrap
