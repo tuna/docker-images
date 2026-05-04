e192f4686047dc7c85e9c969028e191730f67cd2
yaoge123 <my@yaoge123.com>
2026-05-04 10:47:49 +0800

    git-fcgi: allow configuring FastCGI bind address

--- git-fcgi/cmd.sh
+++ git-fcgi/cmd.sh
@@ -2,10 +2,11 @@
 
 THREADS=${THREADS:-"64"}
 QUEUE_SIZE=${QUEUE_SIZE:-"48"}
+FCGI_BIND_ADDRESS=${FCGI_BIND_ADDRESS:-"::"}
 export HOME=/var/www
 (
     while true; do
         /go-queue --queue-size "${QUEUE_SIZE}" --port-number 8888
     done
 )&
-exec /usr/bin/spawn-fcgi -u www-data -g www-data -p 5000 -n -- /usr/bin/multiwatch -f ${THREADS} -- /usr/sbin/fcgiwrap
+exec /usr/bin/spawn-fcgi -u www-data -g www-data -a "${FCGI_BIND_ADDRESS}" -p 5000 -n -- /usr/bin/multiwatch -f ${THREADS} -- /usr/sbin/fcgiwrap
