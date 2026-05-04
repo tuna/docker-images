13a2c9eca55801f5f8d74f89a9eb9dcdd363624f
yaoge123 <my@yaoge123.com>
2026-05-04 20:34:07 +0800

    git-fcgi: keep bind address opt-in

--- git-fcgi/cmd.sh
+++ git-fcgi/cmd.sh
@@ -2,11 +2,16 @@
 
 THREADS=${THREADS:-"64"}
 QUEUE_SIZE=${QUEUE_SIZE:-"48"}
-FCGI_BIND_ADDRESS=${FCGI_BIND_ADDRESS:-"::"}
 export HOME=/var/www
 (
     while true; do
         /go-queue --queue-size "${QUEUE_SIZE}" --port-number 8888
     done
 )&
-exec /usr/bin/spawn-fcgi -u www-data -g www-data -a "${FCGI_BIND_ADDRESS}" -p 5000 -n -- /usr/bin/multiwatch -f ${THREADS} -- /usr/sbin/fcgiwrap
+
+spawn_fcgi_args=(-u www-data -g www-data -p 5000 -n)
+if [[ -n "${FCGI_BIND_ADDRESS:-}" ]]; then
+    spawn_fcgi_args+=(-a "${FCGI_BIND_ADDRESS}")
+fi
+
+exec /usr/bin/spawn-fcgi "${spawn_fcgi_args[@]}" -- /usr/bin/multiwatch -f "${THREADS}" -- /usr/sbin/fcgiwrap
